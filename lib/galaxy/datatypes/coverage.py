9098ee7c931fdb3f46104600d78687303a70ac2e
helingxia <helingxia@grandomics.com>
2020-05-27 11:58:43 +0800

    添加新的datatype——coverage

--- lib/galaxy/datatypes/coverage.py
+++ lib/galaxy/datatypes/coverage.py
@@ -1,32 +1,42 @@
 """
 Coverage datatypes
-
+ 
 """
-
+ 
 import logging
 import math
-
+ 
 from galaxy.datatypes import metadata
+from galaxy.datatypes.sniff import (
+    build_sniff_from_prefix,
+    get_headers,
+    iter_headers,
+)
 from galaxy.datatypes.metadata import MetadataElement
 from galaxy.datatypes.tabular import Tabular
-
+ 
 log = logging.getLogger(__name__)
-
-
-class LastzCoverage(Tabular):
+ 
+class DepthCoverage(Tabular):
+ 
     file_ext = "coverage"
-
+ 
     MetadataElement(name="chromCol", default=1, desc="Chrom column", param=metadata.ColumnParameter)
-    MetadataElement(name="positionCol", default=2, desc="Position column", param=metadata.ColumnParameter)
-    MetadataElement(name="forwardCol", default=3, desc="Forward or aggregate read column", param=metadata.ColumnParameter)
-    MetadataElement(name="reverseCol", desc="Optional reverse read column", param=metadata.ColumnParameter, optional=True, no_value=0)
-    MetadataElement(name="columns", default=3, desc="Number of columns", readonly=True, visible=False)
+    MetadataElement(name="depth", default=2, desc="depth:1000X-0X", param=metadata.ColumnParameter)
+    MetadataElement(name="coverage_percentage", default=3, desc="coverage percentage:0-1", param=metadata.ColumnParameter)
+ 
+    def __init__(self, **kwd):
+        """Initialize DepthCoverage datatype"""
+        Tabular.__init__(self, **kwd)
+ 
+    def sniff(self, filename):
+        headers = get_headers(filename, '\t')
+        try:
+            if len(headers) < 3:
+                return False
+            headers[0] == "MN994467.1"
+        except:
+            return False
+        # If we haven't yet returned False, then...
+        return True
 
-    def get_track_resolution(self, dataset, start, end):
-        range = end - start
-        # Determine appropriate resolution to plot ~1000 points
-        resolution = math.ceil(10 ** math.ceil(math.log10(range / 1000)))
-        # Restrict to valid range
-        resolution = min(resolution, 10000)
-        resolution = max(resolution, 1)
-        return resolution
